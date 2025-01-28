046d3a202e555998fcee69185e13e781808a1b31
Ayoub Nachat <anachat@e3r4p8.1337.ma>
2025-01-28 18:29:23 +0100

    add libs and setup MakeFile

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anachat <anachat@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:29:13 by anachat           #+#    #+#             */
+/*   Updated: 2025/01/28 18:29:13 by anachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int main (int ac, char **av)
+int	main(int ac, char **av)
 {
+	int	i;
+
 	ft_printf("We have %d argumets.\n", ac - 1);
-	int i  = 1;
+	i = 0;
 	while (ac > 1 && i < ac)
 	{
 		ft_printf("Arg N°%d:\n'%s'\n", ac, av[i]);
--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,18 @@
-#include "./lib/libft/libft.h"
-#include "./lib/ft_printf/ft_printf.h"
-#include "./lib/get_next_line/get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anachat <anachat@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:27:04 by anachat           #+#    #+#             */
+/*   Updated: 2025/01/28 18:27:56 by anachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "./lib/libft/libft.h"
+# include "./lib/ft_printf/ft_printf.h"
+# include "./lib/get_next_line/get_next_line.h"
+#endif
